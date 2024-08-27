4c9421e321a97511dfeec17c281f6070f3bf4c7a
ian victor <ianvictorandrade@hotmail.com>
2024-08-27 12:01:35 -0400

    style: adicionado subtitulo na tela inicial

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include('header.php'); ?>
 <div class="container">
-    <h1>Bem-vindo ao Sistema PHP</h1>
+    <h1>Bem-vindos</h1>
+    <h4>Sistema de gerenciamento de produtos e usuários em PHP</h4>
     <p>Escolha uma opção no menu para começar.</p>
 </div>
 <?php include('footer.php'); ?>
